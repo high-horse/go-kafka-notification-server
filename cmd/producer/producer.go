b5e19061647f901624d29728fd503277f44ea14a
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-06-26 17:17:12 +0545

    complete

--- cmd/producer/producer.go
+++ cmd/producer/producer.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	models "kafka-notify/pkg"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -121,4 +122,33 @@ func setupProducer() (sarama.SyncProducer, error) {
 		return nil, err
 	}
 	return producer, nil
+}
+
+
+
+func main() {
+	users := []models.User {
+		{ID: 1, Name: "John"},
+		{ID: 2, Name: "Jane"},
+		{ID: 3, Name: "Jill"},
+		{ID: 4, Name: "Jack"},
+		{ID: 5, Name: "Judy"},
+	}
+
+	producer, err := setupProducer()
+	if err != nil {
+		log.Fatalf("failed to initialize producer: %v", err)
+	}
+
+	defer producer.Close()
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.POST("/send", sendMessageHandler(producer, users))
+
+	fmt.Printf("Kafka PRODUCER 📨 started at http://localhost%s\n", ProducerPort)
+
+	if err := router.Run(ProducerPort); err != nil {
+		log.Printf("failed to run the server: %v", err)
+	}
 }
